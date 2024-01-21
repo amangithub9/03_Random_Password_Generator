cc00d8119d56cf326908e9742c67363b7cc551df
Aman Gupta <115404057+amangithub9@users.noreply.github.com>
2024-01-21 20:34:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const passwordBox = document.getElementById("password");
+const Btn = document.getElementById("btn");
+const copyBtn = document.getElementById("copy-btn");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|{}[]<>/-=";
+
+const allChar = upperCase + lowerCase + number + symbol;
+
+Btn.addEventListener("click", createPassword);
+copyBtn.addEventListener("click", copyPass);
+
+function createPassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while (length > password.length) {
+        password += allChar[Math.floor(Math.random() * allChar.length)]
+    }
+    passwordBox.value = password;
+}
+
+function copyPass() {
+    passwordBox.select();
+    document.execCommand("copy");
+}
